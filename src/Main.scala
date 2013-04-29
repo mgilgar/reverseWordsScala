94df6807221f649548ee760e00b547cecc1f4762
miguel.gil@ukmetro.co.uk <miguel.gil@ukmetro.co.uk>
2013-04-29 11:45:43 +0100

    No comment

--- src/Main.scala
+++ src/Main.scala
@@ -7,31 +7,39 @@ object Main extends App {
 	for (i <- 0 to numberOfCases - 1) {
 	  lines(i) = Console.readLine // () to access the components of an array.
 	}
+	println
 	
-	// Java traditional way
+	// Java traditional way	
+	println ("Java Way")
 	for(line <- lines) {
 	  val words = line.split(" ")
-	  for (i <- words.length - 1 to 0) {
+	  for (i <- 0 to words.length - 1 reverse) {
 	    print(words(i) + " ")
 	  }
 	  println()
 	}
 	
 	// Scala-Java way
+	println
+	println ("Java-Scala way")
 	for (line <- lines) { // for each
 	  val words = line.split(" ")
 	  // 3 different ways of doing the same
-
-	  
 	  words.reverse.map( (m) => {print(m + " ") }) // applying an annonymous function
+	  println
 	  words.reverse.map( (m) => {print(m); print(" ")}) // no need to use () if we are not passing an argument
+	  println
 	  words.reverse.map( print) // easy way to apply a function to the elements of an array without a loop
 	  println
 	}
 	
 	// Scala 100%!
 	// The same in a more concise and scala and functional programming way of doing things
-	lines.map((line) => {
-	  line.split(" ").reverse.map( (m) => {print(m + " ")})
-	})
+	println
+	println("Scala way")
+	lines.map(
+	    (line) => {line.split(" ").reverse.map( 
+	      (w) => {print(w + " ")})
+	      println
+	    })
 }
